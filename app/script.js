89c2f7f8b66e7731501c7e86bd5cf7864dff997c
Javier Arellano <joma_lp_18@hotmail.com>
2019-11-30 12:04:17 -0600

    marca en los li

--- app/script.js
+++ app/script.js
@@ -4,14 +4,16 @@ function cambiar() {
 
     var urlActual = window.location;
     var links = ['index', 'quienes-somos', "donde-estamos", "enlaces"];
-    var temp = "http://127.0.0.1:5500/" + "quienes-somos" + ".html";
-    if (urlActual == temp) {
+    var comp = "http://127.0.0.1:5500/";
+    var temp = comp + "quienes-somos.html",
+        temp2 = comp + "filosofias.html";
+    if (urlActual == temp || urlActual == temp2) {
         nosotros = document.getElementById("nosotros")
         nosotros.className += "actual";
     } else {
         var i = 0;
         for (i = 0; i < links.length; i++) {
-            var urlTemp = "http://127.0.0.1:5500/" + links[i] + ".html";
+            var urlTemp = comp + links[i] + ".html";
             var lista = document.getElementById("menu").children;
             if (urlActual == urlTemp) {
                 lista[i].className += "actual";
