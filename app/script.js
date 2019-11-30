ff77faa9f2cbfee46d81f7b15b35ba729e0c91c5
Javier Arellano <joma_lp_18@hotmail.com>
2019-11-30 11:52:42 -0600

    marca en los li

--- app/script.js
+++ app/script.js
@@ -1,19 +1,25 @@
 window.onload = cambiar;
 
 function cambiar() {
-    /*var urlActual = window.location;
+
+    var urlActual = window.location;
     var links = ['index', 'quienes-somos', "donde-estamos", "enlaces"];
-    var i = 0;
-    for (i = 0; i < links.length; i++) {
-        var urlTemp = "http://127.0.0.1:5500/" + links[i] + ".html";
-        var lista = document.getElementById("menu").children;
-        if (urlActual == urlTemp) {
-            lista[i].className += "actual";
+    var temp = "http://127.0.0.1:5500/" + "quienes-somos" + ".html";
+    if (urlActual == temp) {
+        nosotros = document.getElementById("nosotros")
+        nosotros.className += "actual";
+    } else {
+        var i = 0;
+        for (i = 0; i < links.length; i++) {
+            var urlTemp = "http://127.0.0.1:5500/" + links[i] + ".html";
+            var lista = document.getElementById("menu").children;
+            if (urlActual == urlTemp) {
+                lista[i].className += "actual";
+                console.log("la opcion es: " + urlTemp);
+            }
         }
-    }*/
-    var lis = document.getElementById("menu").getElementsByTagName("li");
-    lis[0].className += "actual";
-    console.log("bla bla " + lis.length);
+    }
+
 }
 
 /*+           var lis = document.getElementById("menu").getElementsByTagName("li");
